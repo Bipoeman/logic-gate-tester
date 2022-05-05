ad36c6ac1a7040fb839e139f1eef7e32406aa674
Bipoe <bipoeman12@gmail.com>
2022-05-05 18:28:40 +0700

    first commit

---
+++ src/header.cpp
@@ -0,0 +1,13 @@
+void resetMCP();
+void Center(String Input, int row);
+void homePage();
+void printIC();
+void writeToReg(uint8_t reg, uint8_t data, int address = 38);
+int readFromReg(uint8_t reg, int address = 38);
+int MCP_pinMode(int pin[], int Mode, int size);
+void MCP_digitalWrite(int pin, int Mode);
+void checkAndShow(CRGB OutOn,CRGB OutOff,CRGB InOn,CRGB InOff);
+void initLED();
+int MCP_DigitalRead(int pinToRead);
+void summaryShow(int counted,int countExpected);
+void Select();
